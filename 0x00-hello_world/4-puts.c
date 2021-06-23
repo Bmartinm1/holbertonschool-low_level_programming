b5ce84d49868626603684cafa0901970d6796b1a
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-23 19:13:28 -0400

    l

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myStr="Programming is like building a multilingual puzzle\n";
+	int puts(myStr);
+
+	return(0);
+}
