00786be26e2df292bb8f4e7bbf0f311f6dcf6728
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 16:16:45 -0400

    first try

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - program entry point
+* _isalpha - checks if target is a lower or uppercase letter
 *
-* Return: always 0
+* @c: target of the checker
+*
+* Return: if _isalpha (1) if ! _isalpha(0)
 **/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
