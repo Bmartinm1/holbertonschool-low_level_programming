f1b664dc9fbc65138bb5c6e818cdedc340ed68c8
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 15:29:26 -0400

    betty formatting

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 **/
 void print_alphabet_x10(void)
 {
-        int count = 0;
+	int count = 0;
 	int alpha;
 
 	while (count < 10)
 	{
-        	for (alpha = 'a'; alpha <= 'z'; alpha++)
-                	_putchar(alpha);
-        	_putchar('\n');
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
 		count++;
 	}
 }
