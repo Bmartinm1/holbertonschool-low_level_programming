b083e1ce9143a352fd2d512999cbe6cc3f8ad32c
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 15:25:39 -0400

    first pass

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - program entry point
+* print_alphabet - prints the alphabet
 *
 * Return: always 0
 **/
+void print_alphabet(void)
+{
+        int count = 0;
+	int alpha;
+
+	while (count < 10)
+	{
+        	for (alpha = 'a'; alpha <= 'z'; alpha++)
+                	_putchar(alpha);
+        	_putchar('\n');
+		count++;
+	}
+}
