98499f3dd8c751adf7854362005cf978be12529b
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 22:22:18 -0400

    first try

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - program entry point
-*
-* Return: always 0
+* print_sign - returns 1, 0, -1, and +, 0, and -, depending on the value of n
+* @n: the target
+* Return: 1, 0, and -1
 **/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
