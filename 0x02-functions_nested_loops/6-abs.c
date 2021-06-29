28997a798f2678ee912ae8355121ae210fc5dfe8
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 23:02:48 -0400

    first pass

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - program entry point
-*
-* Return: always 0
+* _abs - computes the absolute value of an integer
+* @n: target integer
+* Return: absolute value of n
 **/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
