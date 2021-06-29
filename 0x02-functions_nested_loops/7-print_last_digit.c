db48c0b635c0d7579b1523bb753f615d3ab3f28a
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-06-28 23:10:04 -0400

    l

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_last_digit - returns the value of the last digit
+ * @n: target number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+	
+	if (last_digit < 0)
+		last_digit = -last_digit;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
