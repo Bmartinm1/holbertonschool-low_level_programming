b7144ff86e42c9ce0ece68ad78eb38bc27520e6d
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-01 11:13:21 -0400

    first pass

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,17 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
+	largest=a;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (b>largest)
+	{
+		largest=b;
+	}
+	if (c>largest)
+	{
+		largest=c;
+	}
 
-    return (largest);
+	return (largest);
 }
