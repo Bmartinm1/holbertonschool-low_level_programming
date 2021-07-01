421105f5ad17c54630e4fb06033f52b40394595b
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-01 14:41:07 -0400

    first pass

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * int _isupper - checks character for case
+ * @c: character to be checked
+ * Return: 1 if upper, 0 if lower
+ **/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		_putchar('1')
+	else
+		_putchar('0')
+}
