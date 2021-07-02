c9af5d55615ec78e486d86e9eeeecf300917bd51
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-01 23:47:24 -0400

    final push of files

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a diagonal line on terminal
+ * @n: number of times \ is printed
+ *
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = n; i > 0; i--)
+		{
+			for (j = 0; j < (n - i); j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
