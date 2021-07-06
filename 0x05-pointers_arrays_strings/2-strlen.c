f60600bc8e1b15c294f13f7d199965d4cf848d0d
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-06 13:24:26 -0400

    first pass at Q3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+include "holberton.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: pointer to string
+ *
+ */
+int _strlen(char s)
+{
+	int length = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		length++;
+	}
+
+	return (length);
+}
