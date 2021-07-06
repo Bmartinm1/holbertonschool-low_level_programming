6a3b1882b3506ca758dc7f8d3ed03ac0caed7eab
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-06 13:09:39 -0400

    kernel documentation fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * void swap_int - swaps values of two pointers
- * @a, @b: pointers to values that will be swapped
+ * swap_int - swaps values of two pointers
+ * @a: pointer to value for swap
+ * @b: pointer to value for swap
  * Return: always 0
  */
 void swap_int(int *a, int *b)
