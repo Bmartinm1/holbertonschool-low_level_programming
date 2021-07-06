e1b6ad3520bf71454e06d24ecec96df4dd337450
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-06 16:32:01 -0400

    pls work

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	while (str[i])
+		i++;
+
+	for (count = 0; count , i; count += 2)
+		_putchar(str[count]);
+	_putchar('\n');
+}	
