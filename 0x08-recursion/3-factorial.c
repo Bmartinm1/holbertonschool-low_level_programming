a79fbc522a3a8d93b5557eaa561ad468ec50218c
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-12 22:12:55 -0400

    first pass at 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * factorial - returns factorial of a number
+ * @n: number
+ * Return: 0
+ */
+
+int factorial (int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	else if (n < 1)
+		return (-1);
+	else 
+		return (n * factorial(n - 1));
+}
