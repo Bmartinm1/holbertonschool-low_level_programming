962e3d2a48623e1baca6601b19240582a665c519
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-12 22:27:10 -0400

    first pass at 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * sqrt_finder - checks for square root of a number
+ * @a: number
+ * @b: base
+ * Return: square root of b
+ */
+
+int sqrt_finder(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (sqrt_finder(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - returns square root of n
+ * @n: number
+ * Return: square root of n
+ */
+
+int _sqrt_recusion(int n)
+{
+	if(n)
+		return (finder(1, n));
+	else
+		return (0);
+}
