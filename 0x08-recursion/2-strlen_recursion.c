4da4d79647f578b0de1166bc028194516d220dd4
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-12 22:07:49 -0400

    first pass at 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) +1);
+
+	return (0);
+}
