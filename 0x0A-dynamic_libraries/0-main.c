4dcb48b52e84df0654e0ab17630908ed964980e2
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-19 10:47:46 -0400

    renaming files

---
+++ 0x0A-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("Holberton"));
+    return (0);
+}
