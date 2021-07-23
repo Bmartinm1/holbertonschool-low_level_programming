c903f495ff29817cc66a54b54a816c1fd8a09481
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-23 16:06:13 -0400

    second try

--- 0x0B-argc_argv/3-mul.c
+++ 0x0B-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
@@ -26,4 +27,5 @@ int main(int argc, char *argv[])
 		printf("Invalid input, please try again\n");
 
 		return (1);
+	}
 }
