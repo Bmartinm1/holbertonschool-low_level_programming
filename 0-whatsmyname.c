2a008f897deff39cb1cb062f8227710eadc7a115
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-23 15:31:44 -0400

    setup and first question

---
+++ 0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/*
+ * main - program entry point
+ * @argc: num of args
+ * @argv: arg values
+ *
+ * Return 0
+ */
+
+int main(int argc, char* argv[])
+{
+	if(argc)
+		printf("%s\n", argv[0]);
+
+	return 0;
+}
