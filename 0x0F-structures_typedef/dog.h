45b5cbeb019a628aba9dd57d77a95bbcf0251bc3
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-26 21:18:09 -0400

    fixing

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -1,14 +1,14 @@
 #ifndef dog_h
 #define dog_h
 
-typedef struct Dog
+struct Dog
 {
   char *name;
   float age;
   char *owner;
 } dog_h;
 
-// typedef struct Dog
+// struct Dog
 // {
 //   char *name;
 //   float age;
@@ -17,6 +17,6 @@ typedef struct Dog
 
 void init_dog(struct Dog *pup, char *name, float age, char *owner);
 void print_dog(struct Dog *pup);
-dog_h *new_dog(char *name, float age, char *owner);
+// dog_h *new_dog(char *name, float age, char *owner);
 // void free_dog(dog_t *d);
 #endif
