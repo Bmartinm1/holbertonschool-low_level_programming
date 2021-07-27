1ec819e9537d6f7ff2ebfb7f6a5d3214285af654
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-07-26 20:52:03 -0400

    whoops

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -1,19 +1,22 @@
-#include "dog.h"
+#ifndef dog_h
+#define dog_h
 
 /**
- * init_dog - creates a description for a dog
- * @pup: pointer to structure
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
- */
+* struct dog - doggoe description
+* @name: doggoe name
+* @age: doggoe age
+* @owner: of doggoe
+*/
 
-void init_dog(struct Dog *pup, char *name, float age, char *owner)
+typedef struct Dog
 {
-  if (pup)
-  {
-    pup->name = name;
-    pup->age = age;
-    pup->owner = owner;
-  }
-}
+  char *name;
+  float age;
+  char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
