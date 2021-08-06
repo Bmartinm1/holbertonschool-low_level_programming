575481cb391019c4de64ae613eaae2dffd53b384
Ben <ben.martinmooney@gmail.com>
2021-08-06 11:50:55 -0700

    Q0 first try

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list
+ * @h: pointer to list_t
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes = 0;
+	const list_t *temp_list = h;
+
+	while (temp_list)
+	{
+		if (temp_list->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp_list->len, temp_list->str);
+		nodes++;
+		temp_list = temp_list->next;
+	}
+	return (nodes);
+}
