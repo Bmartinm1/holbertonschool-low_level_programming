8dcf4f3ec192e4100c737f652419f267215823dc
Ben <ben.martinmooney@gmail.com>
2021-08-12 07:36:41 -0700

    second pass @ Q: 3

--- 0x14-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,6 +21,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp->n = n;
 	temp->next = NULL;
 
+	if (!*head)
+	{
+		*head = temp;
+		return (temp);
+	}
+
 	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
