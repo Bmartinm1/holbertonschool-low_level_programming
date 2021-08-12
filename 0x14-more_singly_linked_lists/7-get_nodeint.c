5ec35fe7d8aacb84c877a59609063907d5f024c8
Ben <ben.martinmooney@gmail.com>
2021-08-12 12:45:56 -0700

    Q:6

---
+++ 0x14-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns nth node of a listint_t list
+ * @head: list head
+ * @index: node to return
+ *
+ * Return: nth node on success, null on failure
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++
+	}
+
+	return (head)
+}
