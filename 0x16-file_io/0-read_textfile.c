5a55cee181a678da220901b038faf7ee7f12293b
Ben <ben.martinmooney@gmail.com>
2021-08-18 10:47:19 -0700

    wrong file, right code.

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - adds text to the end of file
- * @filename: name of target file
- * @text_content: string to append to file
+ * create_file - creates a file
+ * @filename: name of file pointer
+ * @text_content: pointer to string intended for the new file
  *
- * Return: 1 success, -1 fail
+ * Return: -1 on fail, 1 on success
  */
-
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int status;
@@ -19,16 +18,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	status = write(fd, text_content, strlen(text_content));
-	close(fd);
 
 	if (status == -1)
+	{
+		close(fd);
 		return (-1);
+	}
+	close(fd);
 
 	return (1);
 }
