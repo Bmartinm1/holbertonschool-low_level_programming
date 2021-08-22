c26e84e3138b28c104277e400ce6eecba88f877a
Ben <ben.martinmooney@gmail.com>
2021-08-21 20:39:33 -0700

    removed bang operators

--- 0x10-function_pointers/1-array_iterator.c
+++ 0x10-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t itr;
 
-	if (!action || !array)
+	if (action && array)
 		for (itr = 0; itr < size; itr++)
 			action(array[itr]);
 }
