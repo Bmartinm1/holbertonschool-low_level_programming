ed76ef02cd7c09684479b91f1867cb51d3b6b69b
Ben <ben.martinmooney@gmail.com>
2021-09-09 09:54:50 -0700

    removed semi-colon from end of declaration

--- 0x18-doubly_linked_lists/1-dlistint_len.c
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -7,7 +7,7 @@
  * Return: number of nodes in list or NULL
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *temp;
 	size_t nodeCount = 0;
